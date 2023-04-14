b19a5b9c50b58db63e327e28ef2cae44fbe0a2ce
samndambuki <sam.ndambuki08@gmail.com>
2023-04-14 11:07:12 +0300

    initial commit

---
+++ Question4/question4.js
@@ -0,0 +1,9 @@
+const str = 'Welcome to this Javascript Guide!';
+const reverseSentence = str => {
+   const arr = str.split(" ");
+   const reversed = arr.map(el => {
+      return el.split('').reverse().join("");
+   });
+   return reversed.join(" ");
+};
+console.log(reverseSentence(str));
